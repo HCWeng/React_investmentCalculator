99abf27b540fc39f6d8503ce39d8754e88543390
Andrew Weng <78281843+HCWeng@users.noreply.github.com>
2023-07-24 06:11:18 +0000

    Dynamic code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import ResultsTable from "./components/ResultsTable/ResultsTable";
 import UserInput from "./components/UserInput/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState(null);
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+    setUserInput(userInput);
+  };
+  // Should be triggered when form is submitted
+  // You might not directly want to bind it to the submit event on the form though...
 
-    const yearlyData = []; // per-year results
+  const yearlyData = []; // per-year results
 
+  if (userInput) {
     let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput["expected-return"] / 100;
@@ -26,15 +31,23 @@ function App() {
         yearlyContribution: yearlyContribution,
       });
     }
+  }
 
-    // do something with yearlyData ...
-  };
+  // do something with yearlyData ...
 
   return (
     <div>
       <Header />
       <UserInput onCalculate={calculateHandler} />
-      <ResultsTable />
+      {/* 有資料顯示跟無輸入資料顯示 */}
+      {!userInput && <p>No investment calculated yet.</p>}
+      {/* pass the yearlydata key to the table */}
+      {userInput && (
+        <ResultsTable
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
     </div>
--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,6 +1,12 @@
 import React from "react";
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
 
-function ResultsTable() {
+function ResultsTable(props) {
   return (
     <div>
       {" "}
@@ -15,13 +21,27 @@ function ResultsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
+          {/* 動態顯示tr td  pass data to the App.js datalydata*/}
+          {props.data.map((yearData) => (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+              <td>{formatter.format(yearData.yearlyInterest)}</td>
+              <td>
+                {formatter.format(
+                  yearData.savingsEndOfYear -
+                    props.initialInvestment -
+                    yearData.yearlyContribution * yearData.year
+                )}
+              </td>
+              <td>
+                {formatter.format(
+                  props.initialInvestment +
+                    yearData.yearlyContribution * yearData.year
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
